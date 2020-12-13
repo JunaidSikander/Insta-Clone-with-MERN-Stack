b58be9992f56824db3322412562c28697cba2ac5
Junaid SIkander Hakro <Junaid_sikander@live.com>
2020-12-13 22:42:41 +0500

    [ADD] Mongodb Altus configuration

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const {MONGO_URI} = require('./dev');
 
 const app = express();
 const PORT = 5000;
+
+mongoose.connect(MONGO_URI,{ useUnifiedTopology: true,useNewUrlParser: true })
+  .then(() => console.log("Database Connected"))
+  .catch(err => console.log(err));
 
 app.get('/', (req,res) => {
   res.send("Hello World");
---
+++ dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+  MONGO_URI: 'mongodb+srv://junaid:QBSXRYCCJPha0DJv@instaclone.1o6fe.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
