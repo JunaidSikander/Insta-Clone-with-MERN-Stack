e690a3811d2b47da252daaa37375b62f92b9eaf7
Junaid SIkander Hakro <Junaid_sikander@live.com>
2020-12-14 14:27:35 +0500

    [FIX BUG] database queryTxt ETIMEOUT

--- dev.js
+++ dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-  MONGO_URI: 'mongodb+srv://junaid:QBSXRYCCJPha0DJv@instaclone.1o6fe.mongodb.net/<dbname>?retryWrites=true&w=majority'
+  MONGO_URI: 'mongodb://junaid:QBSXRYCCJPha0DJv@instaclone-shard-00-00.1o6fe.mongodb.net:27017,instaclone-shard-00-01.1o6fe.mongodb.net:27017,instaclone-shard-00-02.1o6fe.mongodb.net:27017/InstaClone?ssl=true&replicaSet=atlas-o2tvu5-shard-0&authSource=admin&retryWrites=true&w=majority'
 }
