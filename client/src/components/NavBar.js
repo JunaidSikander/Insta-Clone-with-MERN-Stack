84e085cf549d6e3a862317b9d1734fc8e9988173
Junaid SIkander Hakro <Junaid_sikander@live.com>
2020-12-16 21:33:25 +0500

    [ADD] materializecss style and navigation bar

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+import '../App.css';
+
+const NavBar = props => (
+    <div>
+        <nav>
+            <div className="nav-wrapper white">
+                <Link to="/" className="brand-logo left">Instagram</Link>
+                <ul id="nav-mobile" className="right">
+                    <li><Link to="/signin">Sign in</Link></li>
+                    <li><Link to="/signup">Sign up</Link></li>
+                    <li><Link to="/profile">Proflie</Link></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+);
+
+export default NavBar;
