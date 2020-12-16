3fc0db3e6975cacb0d2fc3077470e3021b198953
Junaid SIkander Hakro <Junaid_sikander@live.com>
2020-12-16 21:35:27 +0500

    [ADD] basic screens and set their routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import NavBar from "./components/NavBar";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import {Home, Profile, Signin, Signup} from './screens';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Router>
+                <NavBar/>
+                <Route exact path="/" component={Home}/>
+                <Route path="/signin" component={Signin}/>
+                <Route path="/signup" component={Signup}/>
+                <Route path="/profile" component={Profile}/>
+
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ client/src/screens/index.js
@@ -0,0 +1,11 @@
+import Home from "./Home";
+import Profile from "./Profile";
+import Signin from "./Signin";
+import Signup from "./Signup";
+
+export {
+    Home,
+    Profile,
+    Signin,
+    Signup
+};
