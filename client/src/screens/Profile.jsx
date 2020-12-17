073e913b555dcea3f22e84645e109aa9b313ad13
Junaid SIkander Hakro <Junaid_sikander@live.com>
2020-12-17 10:31:20 +0500

    [SET] profile UI

--- client/src/screens/Profile.jsx
+++ client/src/screens/Profile.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
 
 const Profile = () => (
-    <h1>Profile</h1>
+    <div className="profile-container">
+        <div className='profile-subcontainer'>
+            <div>
+                <img className="image-container"
+                     alt=""
+                     src="https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjV8fHBlcnNvbnxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60"
+                />
+            </div>
+            <div>
+                <h4>Junaid Ali</h4>
+                <div className="info-container">
+                    <h6> 40 posts</h6>
+                    <h6> 40 followers</h6>
+                    <h6> 40 following</h6>
+                </div>
+            </div>
+        </div>
+        <div className="gallery">
+            <img className="item"
+                 alt=""
+                 src="https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjV8fHBlcnNvbnxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60"/>
+            <img className="item"
+                 alt=""
+                 src="https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjV8fHBlcnNvbnxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60"/>
+            <img className="item"
+                 alt=""
+                 src="https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjV8fHBlcnNvbnxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60"/>
+            <img className="item"
+                 alt=""
+                 src="https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjV8fHBlcnNvbnxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60"/>
+            <img className="item"
+                 alt=""
+                 src="https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjV8fHBlcnNvbnxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60"/>
+            <img className="item"
+                 alt=""
+                 src="https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjV8fHBlcnNvbnxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60"/>
+        </div>
+    </div>
 );
 
 export default Profile;
