34f3a0dc6d634a060f1b6e314e56b88f68ce6925
Junaid SIkander Hakro <Junaid_sikander@live.com>
2020-12-18 08:12:21 +0500

    [CONNECT] sign up page with backend

--- client/src/screens/Signup.jsx
+++ client/src/screens/Signup.jsx
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from "react-router-dom";
+import authService from "../services/authService";
 
-const Signup = () => (
-    <div className="card-container">
-        <div className="card auth-card input-field">
-            <h2>Instagram</h2>
-            <input type="text" placeholder="name"/>
-            <input type="text" placeholder="email"/>
-            <input type="text" placeholder="password"/>
-            <button className="btn waves-effect waves-light #64b5f6 blue lighten-2" type="submit" name="action">
-                Signup
-            </button>
-            <h5>
-                <Link to="/signin">Already have account</Link>
-            </h5>
+
+const Signup = () => {
+    const [user, setUser] = useState({name: "", email: "", password: ""});
+
+    const onChange = e => {
+        e.preventDefault();
+        setUser({...user, [e.target.name]: e.target.value});
+    };
+
+    const resetForm = () => {
+        setUser({name: "", email: "", password: ""});
+    };
+
+    const onSubmit = e => {
+        e.preventDefault();
+        authService.signUp(user)
+            .then(data => {
+                console.log(data)
+            })
+    };
+    return (
+        <div className="card-container">
+            <div className="card auth-card input-field">
+                <h2>Instagram</h2>
+                <form onSubmit={onSubmit}>
+                    <input type="text" placeholder="name" name='name' value={user.name} onChange={onChange}/>
+                    <input type="text" placeholder="email" name='email' value={user.email} onChange={onChange}
+                           required/>
+                    <input type="password" placeholder="password" name='password' value={user.password}
+                           onChange={onChange} required/>
+                    <button className="btn waves-effect waves-light #64b5f6 blue lighten-2" type="submit">
+                        Signup
+                    </button>
+                    <h5>
+                        <Link to="/signin">Already have account</Link>
+                    </h5>
+                </form>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default Signup;
---
+++ client/src/services/authService.js
@@ -0,0 +1,13 @@
+export default {
+    signUp: async user => {
+        return await fetch('/signup', {
+            method: 'POST',
+            body: JSON.stringify(user),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.json())
+            .then(data => data)
+            .catch(err => console.log(err))
+    }
+};
