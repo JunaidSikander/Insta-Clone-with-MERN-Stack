935cd4f654f193d5f6504467511b5885ca0aa112
Junaid SIkander Hakro <Junaid_sikander@live.com>
2020-12-21 21:29:11 +0500

    [CHANGE] navigation links for authenticated user

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Link} from 'react-router-dom'
+import {AuthContext} from "../context/AuthContext";
 import '../App.css';
 
-const NavBar = props => (
-    <div>
-        <nav>
-            <div className="nav-wrapper white">
-                <Link to="/" className="brand-logo left">Instagram</Link>
-                <ul id="nav-mobile" className="right">
-                    <li><Link to="/signin">Sign in</Link></li>
-                    <li><Link to="/signup">Sign up</Link></li>
-                    <li><Link to="/profile">Proflie</Link></li>
-                    <li><Link to="/upload_post">Upload Posts</Link></li>
-                </ul>
-            </div>
-        </nav>
-    </div>
-);
+const NavBar = props => {
+    const {isAuthenticated} = useContext(AuthContext);
+    const renderList = () => {
+        if (isAuthenticated)
+            return [
+                <li><Link to="/profile">Proflie</Link></li>,
+                <li><Link to="/upload_post">Upload Posts</Link></li>
+            ];
+        else
+            return [
+                <li><Link to="/signin">Sign in</Link></li>,
+                <li><Link to="/signup">Sign up</Link></li>
+            ];
+    };
+    return (
+        <div>
+            <nav>
+                <div className="nav-wrapper white">
+                    <Link to={isAuthenticated ? "/" : "/signin"} className="brand-logo left">Instagram</Link>
+                    <ul id="nav-mobile" className="right">
+                        {renderList()}
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
+};
 
 export default NavBar;
